eb3cc47d447a85a75e1fac1c75844ed2fb271522
Kai Boon Ee <eekaiboon@gmail.com>
2024-07-16 23:09:45 -0700

    unicafe step 2

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -6,8 +6,8 @@ const Button = ( {handleClick, text} ) => (
   </button>
 )
 
-const Display = ( {text, count} ) => (
-  <p>{text} {count}</p>
+const Display = ( {text, value} ) => (
+  <p>{text} {value}</p>
 )
 
 function App() {
@@ -20,6 +20,12 @@ function App() {
     return () => setValue(count + 1)
   }
 
+  // calculate stats
+  const total = good + neutral + bad
+  const score = good * 1 + neutral * 0 + bad * -1
+  const average = score / total
+  const positive = good / total * 100
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -27,9 +33,12 @@ function App() {
       <Button handleClick={incrementCount( {count: neutral, setValue: setNeutral} )} text='neutral' />
       <Button handleClick={incrementCount( {count: bad, setValue: setBad} )} text='bad' />
       <h1>statistics</h1>
-      <Display text='good' count={good} />
-      <Display text='neutral' count={neutral} />
-      <Display text='bad' count={bad} />
+      <Display text='good' value={good} />
+      <Display text='neutral' value={neutral} />
+      <Display text='bad' value={bad} />
+      <Display text='all' value={total} />
+      <Display text='average' value={average} />
+      <Display text='positive' value={positive + ' %'} />
     </div>
   )
 }
