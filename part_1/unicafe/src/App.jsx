61bafaeb5b8f89a9ee027cf0f77a862ff5c821d6
Kai Boon Ee <eekaiboon@gmail.com>
2024-07-17 22:38:45 -0700

    unicafe step 6

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -6,7 +6,12 @@ const Button = ( {handleClick, text} ) => (
   </button>
 )
 
-const StatisticLine = ( {text, value} ) => <p>{text} {value}</p>
+const StatisticLine = ( {text, value} ) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
 
 const Statistics = (props) => {
   if (props.total == 0) {
@@ -17,14 +22,16 @@ const Statistics = (props) => {
     )
   }
   return (
-    <div>
-      <StatisticLine text='good' value={props.good} />
-      <StatisticLine text='neutral' value={props.neutral} />
-      <StatisticLine text='bad' value={props.bad} />
-      <StatisticLine text='all' value={props.total} />
-      <StatisticLine text='average' value={props.average} />
-      <StatisticLine text='positive' value={props.positive + '%'} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={props.good} />
+        <StatisticLine text='neutral' value={props.neutral} />
+        <StatisticLine text='bad' value={props.bad} />
+        <StatisticLine text='all' value={props.total} />
+        <StatisticLine text='average' value={props.average} />
+        <StatisticLine text='positive' value={props.positive + '%'} />
+      </tbody>
+    </table>
   )
 }
 
