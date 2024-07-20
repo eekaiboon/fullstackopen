a2cb4e6c149b27d3c3bb88cc0d258146430ff611
Kai Boon Ee <eekaiboon@gmail.com>
2024-07-19 21:06:45 -0700

    course info step 8

--- part_2/course_info/src/App.jsx
+++ part_2/course_info/src/App.jsx
@@ -9,11 +9,18 @@ const Part = ({ part }) =>
 
 const Content = ({ parts }) => parts.map(part => <Part key={part.id} part={part} />)
 
-const Course = ({course}) =>
-  <>
+const Course = ({course}) => {
+  const total = course.parts.reduce(
+    (accum, curr) => accum + curr.exercises,
+    0
+  )
+
+  return <>
     <Header course={course.name} />
     <Content parts={course.parts} />
+    <Total sum={total} />
   </>
+}
 
 const App = () => {
   const course = {
